2e1e536e2584b5490637812068c14e8402a69f6c
Leamich <tennantonelove@yandex.ru>
2024-02-09 18:36:57 +0400

    main

--- main.py
+++ main.py
@@ -9,5 +9,10 @@ def index():
     return render_template('base.html')
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('')
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
