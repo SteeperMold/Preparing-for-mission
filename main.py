10cae686378c30ce185ab733975422048149772c
Leamich <tennantonelove@yandex.ru>
2024-02-12 18:40:30 +0400

    Цвет каюты!

--- main.py
+++ main.py
@@ -60,5 +60,10 @@ def distribution():
     return render_template('distribution.html', names=names)
 
 
+@app.route('/table/<sex>/<int:age>')
+def table(sex, age):
+    return render_template('table.html', sex=sex, age=age)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
